c468fbc06b36ce19ede3c9d9f6e9dcfaa14c19ee
Sarah <115113929+smclacke@users.noreply.github.com>
2023-03-09 17:59:58 +0100

    Add files via upload

---
+++ ref/exam_02/inter.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+void	ft_inter(char *s1, char *s2)
+{
+	int	i = -1;
+	int	j;
+
+	char tmp[255];
+
+	while (s1[++i])
+	{
+		j = - 1;
+		while (s2[++j])
+		{
+			if (tmp[(int)s1[i]] != 1 && (s1[i] == s2[j]))
+			{
+				write(1, &s1[i], 1);
+				tmp[(int)s1[i]] = 1;
+			}
+		}
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 3)
+		ft_inter(argv[1], argv[2]);
+	write(1, "\n", 1);
+	return (0);
+}
+
+// "nothing" "this sentence hides nothing"
+
+// nothig 
